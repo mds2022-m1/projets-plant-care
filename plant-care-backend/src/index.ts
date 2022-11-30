0ea9b7f86659b38ee37cc43f0e6cc86efd53086d
Antoine <poeuf.alexantoine@orange.fr>
2022-11-30 11:31:50 +0100

    🧪 add test and fix bdd

--- plant-care-backend/src/index.ts
+++ plant-care-backend/src/index.ts
@@ -23,13 +23,39 @@ const AppDataSource = new DataSource({
 AppDataSource.initialize()
     .then(async () => {
         const user = new User()
+        user.uuid_github = 'antoine-github'
         user.name = 'antoine'
         user.password = '123456'
         user.email = 'antoine@orange.fr'
-        user.avatar = 123456
-
+        user.avatar = '123456.png'
         await AppDataSource.manager.save(user)
-    
         console.log(User, "has been saved")
+
+        const place = new Place()
+        place.name = 'salon'
+        place.uuid_user = user
+        await AppDataSource.manager.save(place)
+        console.log(Place, "has been saved")
+
+        const plant = new Plant()
+        plant.uuid_place = place
+        plant.name = 'tulipe'
+        plant.picture = 'image.png'
+        
+        await AppDataSource.manager.save(plant)
+        console.log(Plant, "has been saved")
+
+        let date: Date = new Date();
+
+        const task = new Task()
+        task.uuid_plant = plant
+        task.name = 'arrosage'
+        task.note = 'bien arroser le pied'
+        task.frequency_type = 'Month/Year/Day'
+        task.last_action = date
+        task.month = '[2.3.4]'
+        task.action_frequency = 2
+        await AppDataSource.manager.save(task)
+        console.log(Task, "has been saved")
       })
     .catch((error) => console.log(error))
