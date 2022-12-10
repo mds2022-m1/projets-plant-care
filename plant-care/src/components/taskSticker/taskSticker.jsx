ce7f0e02494cb7b4012ed58ab722a693d9d84e32
Monica Walentek <monica.walentek@sully-group.fr>
2022-12-10 11:57:37 +0100

    💄[STYLE]: finished taskSticker & Calendar components

--- plant-care/src/components/taskSticker/taskSticker.jsx
+++ plant-care/src/components/taskSticker/taskSticker.jsx
@@ -1,10 +1,43 @@
 import { IonCard, IonCardContent, IonCardHeader, IonCol, IonGrid, IonIcon, IonImg, IonRow } from "@ionic/react";
-import { create } from "ionicons/icons";
-import React from "react";
+import { checkmarkCircleOutline, create, time, timeOutline } from "ionicons/icons";
+import moment from 'moment';
+import 'moment/locale/fr'
 import './taskSticker.css';
 
 
 const TaskSticker = ({task, date, plantName, zone, picture}) => {
+
+    const formatDate = (date) => {
+        return upperFirstCase(moment(date, 'YYYY-MM-DD').locale('fr').format('DD MMMM YYYY', 'fr'));
+    }
+
+    const upperFirstCase = (string) => {
+        const arr = string.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+        }
+        const str2 = arr.join(" ");
+        return str2;
+    }
+
+    const isLate = (date) => {
+        const today = moment().format('YYYY-MM-DD');
+        if (date < today) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const showLate = () => {
+        if (isLate(date)) {
+            return (
+                <span className="late">
+                    <IonIcon color="purple" icon={timeOutline} fill="clear"></IonIcon>En retard
+                </span>
+            )
+        }
+    }
     //date = YYYY-MM-DD
     return (
         <>
@@ -14,14 +47,14 @@ const TaskSticker = ({task, date, plantName, zone, picture}) => {
             <IonCardHeader>
                 <IonRow> 
                     <IonCol size={11} class="task"><IonIcon icon={create}></IonIcon>{task}</IonCol>
-                    <IonCol size={1}></IonCol>
+                    <IonCol size={1} className="check-mark"><IonIcon color="mountain" icon={checkmarkCircleOutline}></IonIcon></IonCol>
                 </IonRow>
                 </IonCardHeader>
                 <IonCardContent>
                 <IonRow class="ion-align-items-center">
                 <div className="contain-picture"><img alt="Image de l'utilisateur" src="assets/image/tulipe.png" className="picture"></img></div>
                 <IonCol>
-                    <span className="date">{date}</span> <br/>
+                    <span className="date">{formatDate(date)}</span>{showLate()} <br/>
                     <span className='plant-name'>{plantName}</span> <br/>
                     <div className="zone">Zone : {zone}</div> <br/>
                     </IonCol>
--- plant-care/src/pages/dashboard/Dashboard.tsx
+++ plant-care/src/pages/dashboard/Dashboard.tsx
@@ -1,17 +1,39 @@
-import { IonCard, IonContent, IonHeader, IonItem, IonList, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonContent, IonDatetime, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonModal, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import './Dashboard.css';
 import Select from '../../components/select/Select';
 import TaskSticker from '../../components/taskSticker/taskSticker';
+import { calendar, calendarOutline } from 'ionicons/icons';
+import { useRef } from 'react';
 
-const Dashboard: React.FC = () => {  
+const Dashboard: React.FC = () => {
+
+  const calendarModal = () => {
+  }
+
+  const modal = useRef<HTMLIonModalElement>(null);
+
+  function dismiss() {
+    modal.current?.dismiss();
+  }
+
   return (
     <IonPage>
       <Header />
       <IonContent>
       <Select data={["Aujourd'hui", "Bientôt", "Ce mois-ci"]} defaultValue="Aujourd'hui" handleClick={(e: { target: { value: any; }; }) => console.log(e.target.value)}/>
-      <TaskSticker task="arroser" date="11 Novembre 2022" plantName="Tulipe" zone="Chambre" picture="path/to/image"/>
+      <IonButton fill="clear" id="open-calender" expand="block"><IonIcon icon={calendar} color="gunmetal"></IonIcon></IonButton>
+      {/*Open a modal when we click on the icon of the calendar */}
+      <IonModal id="example-modal" ref={modal} trigger="open-calender">
+        <div className="wrapper">
+          <IonDatetime presentation="date"></IonDatetime>
+        </div>
+      </IonModal>
+
+
+      <TaskSticker task="arroser" date="2022-11-11" plantName="Tulipe" zone="Chambre" picture="path/to/image"/>
+      <TaskSticker task="Tondre la pelouse" date="2022-12-11" plantName="Herbe" zone="Jardin" picture="path/to/image"/>
       </IonContent>
       <Footer />
     </IonPage>
