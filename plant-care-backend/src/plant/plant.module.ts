2fcb198f33b873be9dd58327fb46c027f9905655
Alloula Elias <72727849+flicker71@users.noreply.github.com>
2023-01-16 10:05:58 +0100

    🔨CRUD pour les Users

--- plant-care-backend/src/plant/plant.module.ts
+++ plant-care-backend/src/plant/plant.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Plant } from '../entity/Plant';
+import { User } from '../entity/User';
+import { UserController } from '../user/user.controller';
+import { UserService } from '../user/user.service';
 import { PlantController } from './plant.controller';
 import { PlantService } from './plant.service';
 
 @Module({
-  controllers: [PlantController],
-  providers: [PlantService],
-  imports: [TypeOrmModule.forFeature([Plant])],
-  exports: [PlantService],
+  controllers: [PlantController, UserController],
+  providers: [PlantService, UserService],
+  imports: [TypeOrmModule.forFeature([Plant, User])],
+  exports: [PlantService, UserService],
 })
 export class PlantModule {}
---
+++ plant-care-backend/src/user/user.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entity/User';
+
+@Injectable()
+export class UserService {
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
+
+    public getUser(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
+    public async getUserById(uuid: string): Promise<User> {
+        const user = await this.userRepository.findOne({
+            where: { uuid }
+        });
+        return user;
+    }
+
+    public async createUser(createUserRequest: User): Promise<User> {
+        const user = new User;
+        user.name = createUserRequest.name;
+        user.email = createUserRequest.email;
+        user.password = createUserRequest.password;
+        user.avatar = createUserRequest.avatar;
+
+        await this.userRepository.save(user);
+
+        return user;
+    }
+
+    public async updateUser(uuid: string, updateUserRequest: User): Promise<User> {
+        const user = new User;
+        user.name = updateUserRequest.name;
+        user.email = updateUserRequest.email;
+        user.password = updateUserRequest.password;
+        user.avatar = updateUserRequest.avatar;
+
+        await this.userRepository.update(uuid, user);
+
+        return user;
+    }
+
+    //TODO
+    public async deleteUser(uuid: string){
+        await this.userRepository.delete(uuid);
+    }
+}
