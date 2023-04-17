73fba61beb9dfe29ed3620488abd4986a01a58fa
Monica <monica.walentek@gmail.com>
2023-04-17 20:41:38 +0200

    ✨[FEAT]: add zone page and next button

--- plant-care/src/enum/ZoneEnum.js
+++ plant-care/src/enum/ZoneEnum.js
@@ -1,14 +1,15 @@
+
 export const listZone = [
-    { id: 1, name: 'Cuisine', icon: '&#xe7d3;', className: 'material-icons' }, //soup kitchen
-    { id: 2, name: 'Chambre', icon: '&#xefdf;', className: 'material-icons' }, //bred
-    { id: 3, name: 'Bureau', icon: '&#xef5b;', className: 'material-icons' }, //monitor
-    { id: 4, name: 'Véranda', icon: '&#xe203;', className: 'material-icons' }, //pergola
-    { id: 5, name: 'Entrée', icon: '&#xe77c;', className: 'material-icons' }, //door open
-    { id: 6, name: 'Salon', icon: '&#xefed;', className: 'material-icons' }, //chair
-    { id: 7, name: 'Salle de bain', icon: '&#xea41;', className: 'material-icons' }, //bathtub
-    { id: 8, name: 'Couloir', icon: '&#xf02a;', className: 'material-icons' }, //light
-    { id: 9, name: 'Terrasse', icon: '&#xea42;', className: 'material-icons' }, //deck
-    { id: 10, name: 'Jardin', icon: '&#xe406;', className: 'material-icons' }, //nature
-    { id: 11, name: 'Balcon', icon: '&#xe58f;', className: 'material-icons' }, //balcony
-    { id: 12, name: 'Personnalisée', icon: '&#xe3e3;', className: 'material-icons' }, //filter vintage
+    { id: 1, name: 'Cuisine', codePoint: '&#xe7d3;', className: 'material-icons', iconName: 'soup_kitchen'},
+    { id: 2, name: 'Chambre', codePoint: '&#xefdf;', className: 'material-icons', iconName: 'bed'},
+    { id: 3, name: 'Bureau', codePoint: '&#xef5b;', className: 'material-icons', iconName: 'monitor'},
+    { id: 4, name: 'Véranda', codePoint: '&#xe203;', className: 'material-icons', iconName: 'roofing'},
+    { id: 5, name: 'Entrée', codePoint: '&#xe77c;', className: 'material-icons', iconName: 'meeting_room'},
+    { id: 6, name: 'Salon', codePoint: '&#xefed;', className: 'material-icons', iconName: 'chair'},
+    { id: 7, name: 'Salle de bain', codePoint: '&#xea41;', className: 'material-icons', iconName: 'bathtub'},
+    { id: 8, name: 'Couloir', codePoint: '&#xf02a;', className: 'material-icons', iconName: 'light'},
+    { id: 9, name: 'Terrasse', codePoint: '&#xea42;', className: 'material-icons', iconName: 'deck'},
+    { id: 10, name: 'Jardin', codePoint: '&#xe406;', className: 'material-icons', iconName: 'nature'},
+    { id: 11, name: 'Balcon', codePoint: '&#xe58f;', className: 'material-icons', iconName: 'balcony'},
+    { id: 12, name: 'Personnalisée', codePoint: '&#xe3e3;', className: 'material-icons', iconName: 'filter_vintage'},
 ];
--- plant-care/src/pages/addZone/AddZone.tsx
+++ plant-care/src/pages/addZone/AddZone.tsx
@@ -1,19 +1,85 @@
-import {  IonContent, IonPage } from '@ionic/react';
+import { IonButton, IonContent, IonInput, IonModal, IonPage } from '@ionic/react';
 import './AddZone.css';
 import HeaderReturn from '../../components/headerReturn/HeaderReturn';
 import { listZone } from '../../enum/ZoneEnum';
+import { FocusEvent, useRef, useState } from 'react';
+import Next from '../../components/next/Next';
 
 const AddZone = () => {
 
+  const [zoneSelected, setZoneSelected] = useState<string>('');
+  const [canDismiss, setCanDismiss] = useState<boolean>(false);
 
-  return (
-    <IonPage>
-      <HeaderReturn pageToReturn="/plants" title="Choisir une zone" />
-      <IonContent>
-       
-      </IonContent>
-    </IonPage>
-  );
-};
+  const zones = () => listZone.map((zone) => {
+    return (
+      <IonButton className="zone-btn" fill='clear' key={zone.name} id={zone.name} onClick={() => showZoneSelected(zone.name)}>
+        <div>
+          <i className={zone.className}>{zone.iconName}</i>
+          <div className="zone-txt">{zone.name}</div>
+        </div>
+      </IonButton>
+    );
+  });
 
-export default AddZone;
+  const showZoneSelected = (zoneName: string) => {
+    if (zoneSelected == '') {
+      setZoneSelected(zoneName);
+      document.getElementById(zoneName)?.classList.toggle('zone-btn-selected');
+    } else if (zoneSelected == zoneName) {
+      setZoneSelected('');
+      document.getElementById(zoneName)?.classList.toggle('zone-btn-selected');
+    } else {
+      document.getElementById(zoneSelected)?.classList.toggle('zone-btn-selected');
+      setZoneSelected(zoneName);
+      document.getElementById(zoneName)?.classList.toggle('zone-btn-selected');
+    }
+
+    if (zoneName == 'Personnalisée') {
+      handleChange();
+    }
+
+    
+
+  };
+
+  const changeZoneSelected = (e: any) => {
+    setZoneSelected(e.target.value);
+    console.log(e.target.value)
+  }
+
+  const modal = useRef<HTMLIonModalElement>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleChange = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const modalPersonnalise = () => {
+    return (
+    <IonModal id="modal-zone-perso" isOpen={isOpen}>
+      <div className="">
+        Ajouter une zone personnalisée
+        <IonInput placeholder="Nom de la zone" onBlur={(e) => changeZoneSelected(e)}></IonInput>
+        <Next />
+        <IonButton onClick={() => handleChange()}>Annuler</IonButton>
+      </div>
+    </IonModal>
+    )
+  }
+
+    return (
+      <IonPage>
+        <HeaderReturn pageToReturn="/plants" title="Choisir une zone" />
+        <IonContent>
+          <div className='zone-ctnr'>
+            {zones()}
+            {zoneSelected == '' ? <div></div> : <Next />}
+            {modalPersonnalise()}
+          </div>
+        </IonContent>
+      </IonPage>
+    );
+  };
+
+  export default AddZone;
